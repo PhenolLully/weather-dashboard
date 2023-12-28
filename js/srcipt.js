c525a4cd34d6bf2204c4da8c7788baa55bda73ab
Phenol Lully <lullyp95@gmail.com>
2023-12-28 14:54:12 -0500

    Made changes to JS and HTML

--- js/srcipt.js
+++ js/srcipt.js
@@ -4,30 +4,23 @@ var name = document.querySelector(".name");
 var desc = document.querySelector(".desc");
 var temp = document.querySelector(".temp");
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={c1bc0050e8b1441f96eb602f81355ba2}")
+function searchWeather(){
+    var cityInput = document.querySelector("#cityInput");
+    var latitude = document.querySelector("#latitudeInput") 
+    var longitude = document.querySelector("#longitudeInput") 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=c1bc0050e8b1441f96eb602f81355ba2";
 
-
-var APIKey = "c1bc0050e8b1441f96eb602f81355ba2";
-var city = "";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var url = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=+{API key}"
-fetch(queryURL);
-
-function getApi() {
-  // replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://api.github.com/users/octocat/repos';
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
-    });
+     
+  fetch(queryURL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    
+    name.textContent = data.name;
+    desc.textContent = data.weather[0].description;
+    temp.textContent = data.main.temp;
+  });
 }
 
-fetchButton.addEventListener('click', getApi);
+
